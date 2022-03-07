aafa1aaf4333f7ccfd045309631358f5f8c4b598
Amanda Farrell <bluepandadev@gmail.com>
2022-03-06 18:17:03 -0700

    Initial commit

---
+++ app/src/main/java/com/amandaroos/latlongwithwidget/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.amandaroos.latlongwithwidget
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
