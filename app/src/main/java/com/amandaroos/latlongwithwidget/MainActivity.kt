93e70a2605d79bcc6e4924e72d65cee19a30efd1
Amanda Farrell <bluepandadev@gmail.com>
2022-03-10 21:46:59 -0700

    Add play-services-location:19.0.1 dependency

--- app/src/main/java/com/amandaroos/latlongwithwidget/MainActivity.kt
+++ app/src/main/java/com/amandaroos/latlongwithwidget/MainActivity.kt
@@ -1,11 +1,54 @@
 package com.amandaroos.latlongwithwidget
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.location.Location
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.app.ActivityCompat
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 
 class MainActivity : AppCompatActivity() {
+
+    val RequestPermissionCode = 1
+    var mLocation: Location? = null
+    private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
+        getLastLocation()
+    }
+
+    fun getLastLocation() {
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            requestPermission()
+        } else {
+            fusedLocationProviderClient.lastLocation
+                .addOnSuccessListener { location: Location? ->
+                    mLocation = location
+                    if (location != null) {
+                        latitude.text = location.latitude.toString()
+                        longitude.text = location.longitude.toString()
+                        time.text = android.text.format.DateFormat.getTimeFormat(applicationContext).format(location.time)
+                        date.text = android.text.format.DateFormat.getDateFormat(getApplicationContext()).format(location.time)
+                    }
+                }
+        }
+    }
+
+    private fun requestPermission() {
+        ActivityCompat.requestPermissions(
+            this,
+            arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+            RequestPermissionCode
+        )
     }
 }
